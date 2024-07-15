f3f004ee409e357720b9953199e659918fd25ef3
verynicetomato <verynicetomato@naver.com>
2024-07-15 19:23:08 +0900

    RETRY TEST

--- board/serializers.py
+++ board/serializers.py
@@ -78,3 +78,27 @@ class MovieSerializer(serializers.ModelSerializer):
 
         
 
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    nickname = serializers.CharField(source='user.profile.nickname', read_only=True)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'user', 'nickname', 'title', 'body', 'created_at']
+        extra_kwargs = {
+            'user': {'read_only': True}
+        }
+
+class CommentSerializer(serializers.ModelSerializer):
+    nickname = serializers.CharField(source='user.profile.nickname', read_only=True)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'nickname', 'comment', 'created_at']
+        extra_kwargs = {
+            'user': {'read_only': True}
+        }
